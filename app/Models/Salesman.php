c00e9c3b002e081dc763b288b65b3c2e806561d0
aleonidas <anderson.leonidas@gmail.com>
2018-06-28 18:02:48 -0300

    Process SalesmanModel

--- app/Models/Salesman.php
+++ app/Models/Salesman.php
@@ -7,32 +7,37 @@ class Salesman
 
     public static function getQuantity(array $file_processed)
     {
-        $quantity = 0;
-        foreach ($file_processed as $proc) {
-            if (isset($proc['salesman'])) {
-                $quantity++;
-            }
-        }
-
-        return $quantity;
+        $process = self::process($file_processed);
+        return $process['quantity'];
     }
 
     public static function averageSalaryOfSellers(array $file_processed)
     {
-        $total_salary = 0;
-        $quantity_salesman = self::getQuantity($file_processed);
+        $process = self::process($file_processed);
 
-        if ($quantity_salesman <= 0) {
+        if ($process['quantity'] <= 0) {
             return 0;
         }
 
+        return number_format(($process['total_salary'] / $process['quantity']), 2, '.', '');
+    }
+
+    protected static function process(array $file_processed)
+    {
+        $quantity = 0;
+        $total_salary = 0;
+
         foreach ($file_processed as $proc) {
             if (isset($proc['salesman'])) {
+                $quantity++;
                 $total_salary += floatval($proc['salesman']['salary']);
             }
         }
 
-        return number_format(($total_salary / $quantity_salesman), 2, '.', '');
+        return [
+            'quantity'     => $quantity,
+            'total_salary' => $total_salary
+        ];
     }
 
 }
