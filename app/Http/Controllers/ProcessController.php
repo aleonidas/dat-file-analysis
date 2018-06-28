2615923c9d0daa2ab768913e76ef4a41787b251a
aleonidas <anderson.leonidas@gmail.com>
2018-06-28 17:26:04 -0300

    Sales model

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -10,8 +10,8 @@ use App\Data\Salesman;
 use App\Http\Upload\File;
 use Illuminate\Http\Request;
 use App\Models\Salesman as ModelSalesman;
-use App\Models\Salesman as ModelCustomer;
-
+use App\Models\Customer as ModelCustomer;
+use App\Models\Sales as ModelSales;
 
 class ProcessController extends Controller
 {
@@ -35,10 +35,9 @@ class ProcessController extends Controller
         $processed_information = [
             'quantity_salesman'         => ModelSalesman::getQuantity($processed),
             'quantity_customer'         => ModelCustomer::getQuantity($processed),
-            'average_salary_of_sellers' => $this->averageSalaryOfSellers($processed),
-            'id_best_selling'           => $this->bestSelling($processed),
-            'id_worst_seller'           => $this->worstSeller($processed)
-
+            'average_salary_of_sellers' => ModelSalesman::averageSalaryOfSellers($processed),
+            'id_best_selling'           => ModelSales::bestSelling($processed),
+            'id_worst_seller'           => ModelSales::worstSeller($processed)
         ];
 
 
@@ -62,55 +61,4 @@ class ProcessController extends Controller
         $to = $destiny . DIRECTORY_SEPARATOR . $basename;
         return rename($source, $to);
     }
-
-    public function averageSalaryOfSellers($processed)
-    {
-        $quantity_salesman = $this->getQuantityOfPerson($processed, 'salesman');
-        $total_salary = 0;
-
-        foreach ($processed as $proc) {
-            if (isset($proc['salesman'])) {
-                $total_salary += floatval($proc['salesman']['salary']);
-            }
-        }
-
-        return number_format(($total_salary / $quantity_salesman), 2, '.', '');
-    }
-
-    public function bestSelling($processed)
-    {
-        $sale_id = null;
-        $sale_total = 0;
-
-        foreach ($processed as $proc) {
-            if (isset($proc['sales'])) {
-                if ($proc['sales']['total'] > $sale_total) {
-                    $sale_total = $proc['sales']['total'];
-                    $sale_id   = $proc['sales']['sale_id'];
-                }
-            }
-        }
-
-        return $sale_id;
-    }
-
-    public function worstSeller($processed)
-    {
-        $salesman_id = null;
-        $sale_total = null;
-
-        foreach ($processed as $proc) {
-            if (isset($proc['sales'])) {
-                $sale_total = (is_null($sale_total) ? $proc['sales']['total'] : $sale_total);
-
-                if ($proc['sales']['total'] < $sale_total) {
-                    $sale_total  = $proc['sales']['total'];
-                    $salesman_id = $proc['sales']['salesman_id'];
-                }
-            }
-        }
-
-        return $salesman_id;
-    }
-
 }
--- app/Models/Salesman.php
+++ app/Models/Salesman.php
@@ -17,4 +17,22 @@ class Salesman
         return $quantity;
     }
 
+    public static function averageSalaryOfSellers(array $file_processed)
+    {
+        $total_salary = 0;
+        $quantity_salesman = self::getQuantity($file_processed);
+
+        if ($quantity_salesman <= 0) {
+            return 0;
+        }
+
+        foreach ($file_processed as $proc) {
+            if (isset($proc['salesman'])) {
+                $total_salary += floatval($proc['salesman']['salary']);
+            }
+        }
+
+        return number_format(($total_salary / $quantity_salesman), 2, '.', '');
+    }
+
 }
